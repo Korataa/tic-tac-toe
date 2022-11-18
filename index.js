26e6a368a4e360f455be34227b421f1d18a18c1d
Christopher Goodburn <c.lg11@yahoo.com>
2022-11-17 23:26:00 -0800

    make game playable, just need to fix bugs now

--- index.js
+++ index.js
@@ -10,92 +10,143 @@ let button9 = document.getElementById("button9");
 
 let isX = true;
 
-button1.addEventListener("click", (event) => {
-    if(isX) {
-        event.target.innerHTML = "x";
-    } else {
-        event.target.innerHTML = "o";
+function checkIfWin() {
+    let button1Text = button1.innerHTML;
+    let button2Text = button2.innerHTML;
+    let button3Text = button3.innerHTML;
+    let button4Text = button4.innerHTML;
+    let button5Text = button5.innerHTML;
+    let button6Text = button6.innerHTML;
+    let button7Text = button7.innerHTML;
+    let button8Text = button8.innerHTML;
+    let button9Text = button9.innerHTML;
+
+    //First row horizontal
+    if(button1Text === "x" && button2Text === "x" && button3Text === "x")
+    {
+        alert("You win!");
     }
 
-    isX = !isX;
-});
-
-button2.addEventListener("click", (event) => {
-    if(isX) {
-        event.target.innerHTML = "x";
-    } else {
-        event.target.innerHTML = "o";
+    //second row horizontal
+    if(button4Text === "x" && button5Text === "x" && button6Text === "x")
+    {
+        alert("you win!");
     }
 
-    isX = !isX;
-});
-
-button3.addEventListener("click", (event) => {
-    if(isX) {
-        event.target.innerHTML = "x";
-    } else {
-        event.target.innerHTML = "o";
+    //third row horizontal
+    if(button7Text === "x" && button8Text === "x" && button9Text === "x")
+    {
+        alert("you win!");
     }
 
-    isX = !isX;
-});
-
-button4.addEventListener("click", (event) => {
-    if(isX) {
-        event.target.innerHTML = "x";
-    } else {
-        event.target.innerHTML = "o";
+    //first column vertical
+    if(button1Text === "x" && button4Text === "x" && button7Text === "x")
+    {
+        alert("you win!");
     }
 
-    isX = !isX;
-});
-
-button5.addEventListener("click", (event) => {
-    if(isX) {
-        event.target.innerHTML = "x";
-    } else {
-        event.target.innerHTML = "o";
+    //second column vertical
+    if(button2Text === "x" && button5Text === "x" && button8Text === "x")
+    {
+        alert("you win!");
     }
 
-    isX = !isX;
-});
-
-button6.addEventListener("click", (event) => {
-    if(isX) {
-        event.target.innerHTML = "x";
-    } else {
-        event.target.innerHTML = "o";
+    //third column vertical
+    if(button3Text === "x" && button6Text === "x" && button9Text === "x")
+    {
+        alert("you win!");
+    }
+    //right diagonal
+    if(button1Text === "x" && button5Text === "x" && button9Text === "x")
+    {
+        alert("you win!");
     }
 
-    isX = !isX;
-});
-
-button7.addEventListener("click", (event) => {
-    if(isX) {
-        event.target.innerHTML = "x";
-    } else {
-        event.target.innerHTML = "o";
+    //left diagonal
+    if(button3Text === "x" && button5Text === "x" && button7Text === "x")
+    {
+        alert("you win!");
     }
 
-    isX = !isX;
-});
+    //THE O'S
 
-button8.addEventListener("click", (event) => {
-    if(isX) {
-        event.target.innerHTML = "x";
-    } else {
-        event.target.innerHTML = "o";
+    //First row horizontal
+    if(button1Text === "o" && button2Text === "o" && button3Text === "o")
+    {
+        alert("Os win!");
     }
 
-    isX = !isX;
-});
-
-button9.addEventListener("click", (event) => {
-    if(isX) {
-        event.target.innerHTML = "x";
-    } else {
-        event.target.innerHTML = "o";
+    //second row horizontal
+    if(button4Text === "o" && button5Text === "o" && button6Text === "o")
+    {
+        alert("Os win!");
     }
 
-    isX = !isX;
-});
+    //third row horizontal
+    if(button7Text === "o" && button8Text === "o" && button9Text === "o")
+    {
+        alert("Os win!");
+    }
+
+    //first column vertical
+    if(button1Text === "o" && button4Text === "o" && button7Text === "o")
+    {
+        alert("Os win!");
+    }
+
+    //second column vertical
+    if(button2Text === "o" && button5Text === "o" && button8Text === "o")
+    {
+        alert("Os win!");
+    }
+
+    //third column vertical
+    if(button3Text === "o" && button6Text === "o" && button9Text === "o")
+    {
+        alert("Os win!");
+    }
+    //right diagonal
+    if(button1Text === "o" && button5Text === "o" && button9Text === "o")
+    {
+        alert("Os win!");
+    }
+
+    //left diagonal
+    if(button3Text === "o" && button5Text === "o" && button7Text === "o")
+    {
+        alert("Os win!");
+    }
+}
+
+//mark a box where you're going to move
+function markBox(event) {
+    if(isX && event.target.innerHTML === "") {
+        event.target.innerHTML = "x";
+        isX = !isX;
+    } else if(event.target.innerHTML === "") {
+        event.target.innerHTML = "o";
+        isX = !isX;
+    } else {
+        alert("Can't move there!");
+    }
+
+    checkIfWin();
+}
+
+button1.addEventListener("click", markBox);
+
+button2.addEventListener("click", markBox);
+
+button3.addEventListener("click", markBox);
+
+button4.addEventListener("click", markBox);
+
+button5.addEventListener("click", markBox);
+
+button6.addEventListener("click", markBox);
+
+button7.addEventListener("click", markBox);
+
+button8.addEventListener("click", markBox);
+
+button9.addEventListener("click", markBox);
