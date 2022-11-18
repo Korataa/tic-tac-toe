166feadcae7e6f81c40a0c14ac0c315b1e26fae6
Chris Goodburn <88125326+Korataa@users.noreply.github.com>
2022-11-17 19:30:55 -0800

    add html, css, js

---
+++ index.js
@@ -0,0 +1,101 @@
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+let button4 = document.getElementById("button4");
+let button5 = document.getElementById("button5");
+let button6 = document.getElementById("button6");
+let button7 = document.getElementById("button7");
+let button8 = document.getElementById("button8");
+let button9 = document.getElementById("button9");
+
+let isX = true;
+
+button1.addEventListener("click", (event) => {
+    if(isX) {
+        event.target.innerHTML = "x";
+    } else {
+        event.target.innerHTML = "o";
+    }
+
+    isX = !isX;
+});
+
+button2.addEventListener("click", (event) => {
+    if(isX) {
+        event.target.innerHTML = "x";
+    } else {
+        event.target.innerHTML = "o";
+    }
+
+    isX = !isX;
+});
+
+button3.addEventListener("click", (event) => {
+    if(isX) {
+        event.target.innerHTML = "x";
+    } else {
+        event.target.innerHTML = "o";
+    }
+
+    isX = !isX;
+});
+
+button4.addEventListener("click", (event) => {
+    if(isX) {
+        event.target.innerHTML = "x";
+    } else {
+        event.target.innerHTML = "o";
+    }
+
+    isX = !isX;
+});
+
+button5.addEventListener("click", (event) => {
+    if(isX) {
+        event.target.innerHTML = "x";
+    } else {
+        event.target.innerHTML = "o";
+    }
+
+    isX = !isX;
+});
+
+button6.addEventListener("click", (event) => {
+    if(isX) {
+        event.target.innerHTML = "x";
+    } else {
+        event.target.innerHTML = "o";
+    }
+
+    isX = !isX;
+});
+
+button7.addEventListener("click", (event) => {
+    if(isX) {
+        event.target.innerHTML = "x";
+    } else {
+        event.target.innerHTML = "o";
+    }
+
+    isX = !isX;
+});
+
+button8.addEventListener("click", (event) => {
+    if(isX) {
+        event.target.innerHTML = "x";
+    } else {
+        event.target.innerHTML = "o";
+    }
+
+    isX = !isX;
+});
+
+button9.addEventListener("click", (event) => {
+    if(isX) {
+        event.target.innerHTML = "x";
+    } else {
+        event.target.innerHTML = "o";
+    }
+
+    isX = !isX;
+});
